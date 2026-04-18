ebf073ea613c7e837810b58b427d3dc3389a0144
katheroine <katheroine@gmail.com>
2026-04-18 15:38:30 +0200

    testLogLogsMessageWithoutContext: fail

---
+++ src/Logger.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Copyright (C) 2026 Katarzyna Krasińska
+ * PHP.PSR-3.lab - https://github.com/katheroine/php.psr-3.lab
+ * Licensed under GPL-3.0 - see LICENSE.md
+ */
+
+declare(strict_types=1);
+
+namespace PHPLab\StandardPSR3;
+
+class Logger
+{
+}
