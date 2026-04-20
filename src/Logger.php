de102642810e9a98044e37b8e4161544d9b8213a
katheroine <katheroine@gmail.com>
2026-04-20 11:14:47 +0200

    levelSpecificMethodLogsWithProperLevel: pass

--- src/Logger.php
+++ src/Logger.php
@@ -10,6 +10,8 @@ declare(strict_types=1);
 
 namespace PHPLab\StandardPSR3;
 
+use Psr\Log\LogLevel;
+
 class Logger
 {
     /**
@@ -25,6 +27,121 @@ class Logger
     }
 
     /**
+     * System is unusable.
+     *
+     * @param string|\Stringable $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public function emergency(string|\Stringable $message, array $context = []): void
+    {
+        $this->log(LogLevel::EMERGENCY, $message, $context);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string|\Stringable $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public function alert(string|\Stringable $message, array $context = []): void
+    {
+        $this->log(LogLevel::ALERT, $message, $context);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string|\Stringable $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public function critical(string|\Stringable $message, array $context = []): void
+    {
+        $this->log(LogLevel::CRITICAL, $message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string|\Stringable $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public function error(string|\Stringable $message, array $context = []): void
+    {
+        $this->log(LogLevel::ERROR, $message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string|\Stringable $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public function warning(string|\Stringable $message, array $context = []): void
+    {
+        $this->log(LogLevel::WARNING, $message, $context);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string|\Stringable $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public function notice(string|\Stringable $message, array $context = []): void
+    {
+        $this->log(LogLevel::NOTICE, $message, $context);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string|\Stringable $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public function info(string|\Stringable $message, array $context = []): void
+    {
+        $this->log(LogLevel::INFO, $message, $context);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string|\Stringable $message
+     * @param mixed[] $context
+     *
+     * @return void
+     */
+    public function debug(string|\Stringable $message, array $context = []): void
+    {
+        $this->log(LogLevel::DEBUG, $message, $context);
+    }
+
+    /**
      * Logs with an arbitrary level.
      *
      * @param mixed $level
@@ -32,10 +149,12 @@ class Logger
      * @param array $context
      *
      * @return void
+     *
+     * @throws \Psr\Log\InvalidArgumentException
      */
     public function log(mixed $level, string|\Stringable $message, array $context = []): void
     {
-        if (! in_array($level, array_values(new \ReflectionClass(\Psr\Log\LogLevel::class)->getConstants()))) {
+        if (! in_array($level, array_values(new \ReflectionClass(LogLevel::class)->getConstants()))) {
             throw new \Psr\Log\InvalidArgumentException($level . ' signalisation level is improper.');
         }
 
